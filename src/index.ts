e25026f8a20382239dbb99f27461c8141a721e0d
aleksey <leha.sakun@gmail.com>
2021-12-14 16:56:05 +0200

    feat: add endpoints

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import express, {ErrorRequestHandler} from "express";
+import axios from "axios";
+import path from "path";
+import {promises as fs} from "fs";
+import {NextFunction, Request, Response} from "express/ts4.0";
+
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+
+app.get("/double/:number", (req:Request,res:Response, next:NextFunction)=> {
+  try {
+    res.json({result: Number(req.params.number) * 2 });
+  } catch(err) {
+    next(err);
+  }
+});
+
+app.get('/count', async (req:Request,res:Response, next:NextFunction)=> {
+  const baseDir = path.join(process.cwd());
+  const filePath = `${baseDir}/.count.txt`;
+  try {
+    const data = await fs.readFile(filePath, "utf8").catch(err => {
+      if (err.code === 'ENOENT') {
+        fs.writeFile(filePath, '1', 'utf-8');
+
+        res.json({count: 1});
+      }
+    });
+    if(data) {
+      const parseData = Number(data);
+      const newCount = parseData + 1;
+      await fs.writeFile(filePath, '' + newCount);
+
+      res.json({ count: newCount });
+    }
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.get('/joke', async(req:Request,res:Response, next:NextFunction)=> {
+  try {
+    const resp = await axios.get('https://api.jokes.one/jod');
+    const joke = resp.data.contents;
+    res.json({ joke });
+  } catch(err) {
+    next(err);
+  }
+});
+
+app.use(function (req:Request,res:Response) {
+  res.status(404).send("Not Found");
+});
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+  next(err) ;
+};
+app.use(errorHandler);
+
+
+app.listen(port, () => console.log(`Running on port ${port}`));
